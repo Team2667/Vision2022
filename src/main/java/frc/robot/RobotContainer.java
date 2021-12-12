331fcfc29c30f9615559c361a119fbe37e335f7b
kentbraaten <kentbraaten@frontiernet.net>
2021-12-12 12:03:52 -0600

    Create an XboxController object before bindings.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -26,8 +26,8 @@ public class RobotContainer {
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
-    configureButtonBindings();
     joy = new XboxController(0);
+    configureButtonBindings();
   }
 
   /**
